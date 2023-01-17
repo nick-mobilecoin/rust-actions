260a1b50a48e940c9ebfc058774bd05a5ec49125
Nick Santana <nick@mobilecoin.com>
2023-01-17 15:04:07 -0800

    Have a nonsense timer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::time::SystemTime;
+
+
 fn main() {
+    let start = SystemTime::now(); let delta = start.elapsed();
+    println!("The delta is {delta:?}");
+
     println!("Hello, world!");
 }
