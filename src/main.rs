ecac2494c9894c3d0a494078dd00fb1e051f255a
Lint Action <lint-action@samuelmeuli.com>
2023-01-18 18:22:43 +0000

    Fix code style issues with rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::time::SystemTime;
 
-
 fn main() {
-    let start = SystemTime::now(); let delta = start.elapsed();
+    let start = SystemTime::now();
+    let delta = start.elapsed();
     println!("The delta is {delta:?}");
 
     println!("Hello, world!");
