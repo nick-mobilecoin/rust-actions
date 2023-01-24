e47a51827df7351573c1518799c43e6c3979fe72
Nick Santana <nick@mobilecoin.com>
2023-01-24 06:53:53 -0800

    Popping stash
    adding back format to run on windows and ubuntu

--- src/foo.rs
+++ src/foo.rs
@@ -1,6 +1,6 @@
 use std::time::{SystemTime, Duration};
 
 pub fn delta() -> Duration {
-        let start = SystemTime::now(); 
-        start.elapsed().unwrap()
+        let start = SystemTime::now(); let delta = start.elapsed().unwrap();
+        delta
 }
